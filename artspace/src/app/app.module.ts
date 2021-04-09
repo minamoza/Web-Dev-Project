c7bc9cd319f54574f451feee9342528a80f82eea
Erkebulan <mk.erkebulan@gmail.com>
2021-04-09 17:11:43 +0600

    created base of homepage

--- artspace/src/app/app.module.ts
+++ artspace/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-
-import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { HomeComponent } from './components/home/home.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { AboutComponent } from './components/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -23,12 +22,14 @@ import { AboutComponent } from './components/about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatGridListModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
       {path: 'categories', component: CategoriesComponent},
       {path: 'about', component: AboutComponent},
       {path: '', redirectTo: 'home', pathMatch: 'full'},
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- artspace/src/app/components/home/home.component.ts
+++ artspace/src/app/components/home/home.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public list: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.list = [
+      { rows: 1, cols: 1, data: 1 },
+      { rows: 1, cols: 1, data: 2 },
+      { rows: 1, cols: 1, data: 3 },
+      { rows: 1, cols: 1, data: 4 },
+
+      { rows: 1, cols: 1, data: 5 },
+      { rows: 1, cols: 1, data: 6 },
+      { rows: 1, cols: 1, data: 7 },
+      { rows: 1, cols: 1, data: 8 },
+      { rows: 1, cols: 1, data: 9 },
+
+      { rows: 1, cols: 1, data: 10 },
+      { rows: 1, cols: 1, data: 11 },
+      { rows: 1, cols: 2, data: 12 },
+      { rows: 1, cols: 1, data: 13 },
+      { rows: 1, cols: 1, data: 14 },
+
+      { rows: 1, cols: 1, data: 1 },
+      { rows: 1, cols: 1, data: 2 },
+      { rows: 1, cols: 1, data: 3 },
+      { rows: 1, cols: 1, data: 4 },
+
+      { rows: 1, cols: 1, data: 5 },
+      { rows: 1, cols: 1, data: 6 },
+      { rows: 1, cols: 1, data: 7 },
+      { rows: 1, cols: 1, data: 8 },
+      { rows: 1, cols: 1, data: 9 },
+
+      { rows: 1, cols: 1, data: 10 },
+      { rows: 1, cols: 1, data: 11 },
+      { rows: 1, cols: 2, data: 12 },
+      { rows: 1, cols: 1, data: 13 },
+      { rows: 1, cols: 1, data: 14 },
+    ]
   }
 
 }
